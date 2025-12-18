1a1c0e4eed62899da03f52dda48dcb089bc0992d
darrenkuro <odon5ht@gmail.com>
2025-12-18 19:36:08 +0100

    Update comment in Grace.c

--- c/Grace.c
+++ c/Grace.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 #define FILE "Grace_kid.c"
-#define CODE "#include <stdio.h>%1$c%1$c#define FILE %2$cGrace_kid.c%2$c%1$c#define CODE %2$c%3$s%2$c%1$c#define MAIN int main(void) {fprintf(fopen(FILE, %2$cw%2$c), CODE, 10, 34, CODE);}%1$c%1$cMAIN // Comment%1$c"
-#define MAIN int main(void) {fprintf(fopen(FILE, "w"), CODE, 10, 34, CODE);}
+#define CODE "#include <stdio.h>%1$c%1$c#define FILE %2$cGrace_kid.c%2$c%1$c#define CODE %2$c%3$s%2$c%1$c#define MAIN(x) int main(void) {fprintf(fopen(x, %2$cw%2$c), CODE, 10, 34, CODE);}%1$c%1$cMAIN(FILE)%1$c/*%1$c    Comment%1$c*/%1$c"
+#define MAIN(x) int main(void) {fprintf(fopen(x, "w"), CODE, 10, 34, CODE);}
 
-MAIN // Comment
+MAIN(FILE)
+/*
+    Comment
+*/
