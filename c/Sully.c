14f3f3094419d326cecf426ee1bb4b8546be2ba7
darrenkuro <odon5ht@gmail.com>
2025-12-25 18:57:09 +0100

    Improve on Sully.c

--- c/Sully.c
+++ c/Sully.c
@@ -4,24 +4,17 @@
 #include <unistd.h>
 
 int main() {
-    int i = 5, ret = 1, fd = -1;
-    if (--i < 0) return (ret);
+    char fname[64], cmd[128];
+    int i = 5;
+    if (i < 0) return (0);
 
-    char *fname = NULL, *cmd = NULL;
-    char *code = "#include <stdlib.h>%1$c#include <stdio.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$c%1$cint main() {%1$c    int i = %2$d, ret = 1, fd = -1;%1$c    if (--i < 0) return (ret);%1$c%1$c    char *fname = NULL, *cmd = NULL;%1$c    char *code = %3$c%4$s%3$c;%1$c    char *code_cmd = %3$c%5$s%3$c;%1$c    char *code_fname = %3$c%6$s%3$c;%1$c    if (asprintf(&cmd, code_cmd, i) == -1) goto cleanup;%1$c    if (asprintf(&fname, code_fname, i) == -1) goto cleanup;%1$c%1$c    fd = open(fname, O_WRONLY | O_CREAT | O_TRUNC, 0644);%1$c    if (fd < 0) goto cleanup;%1$c    dprintf(fd, code, 10, i, 34, code, code_cmd, code_fname);%1$c    system(cmd);%1$c    ret = 0;%1$c%1$ccleanup:%1$c    free(fname), free(cmd);%1$c    if (fd >= 0) close(fd);%1$c    return ret;%1$c}%1$c";
-    char *code_cmd = "cc -Wall -Werror -Wextra Sully_%1$d.c -o Sully_%1$d; ./Sully_%1$d";
-    char *code_fname = "Sully_%d.c";
-    if (asprintf(&cmd, code_cmd, i) == -1) goto cleanup;
-    if (asprintf(&fname, code_fname, i) == -1) goto cleanup;
+    char *code = "#include <stdlib.h>%1$c#include <stdio.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$c%1$cint main() {%1$c    char fname[64], cmd[128];%1$c    int i = %2$d;%1$c    if (i < 0) return (0);%1$c%1$c    char *code = %3$c%4$s%3$c;%1$c    snprintf(fname, sizeof fname, %3$cSully_%%d.c%3$c, i);%1$c    int fd = open(fname, O_WRONLY | O_CREAT | O_TRUNC, 0644);%1$c    if (fd < 0) return (1);%1$c%1$c    snprintf(cmd, sizeof cmd, %3$ccc -Wall -Werror -Wextra Sully_%%1$d.c -o Sully_%%1$d; ./Sully_%%1$d%3$c, i);%1$c    dprintf(fd, code, 10, i - 1, 34, code);%1$c    system(cmd);%1$c    return (close(fd), 0);%1$c}%1$c";
+    snprintf(fname, sizeof fname, "Sully_%d.c", i);
+    int fd = open(fname, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd < 0) return (1);
 
-    fd = open(fname, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (fd < 0) goto cleanup;
-    dprintf(fd, code, 10, i, 34, code, code_cmd, code_fname);
+    snprintf(cmd, sizeof cmd, "cc -Wall -Werror -Wextra Sully_%1$d.c -o Sully_%1$d; ./Sully_%1$d", i);
+    dprintf(fd, code, 10, i - 1, 34, code);
     system(cmd);
-    ret = 0;
-
-cleanup:
-    free(fname), free(cmd);
-    if (fd >= 0) close(fd);
-    return ret;
+    return (close(fd), 0);
 }
