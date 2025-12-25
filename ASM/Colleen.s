4be614c614336f83c1aac7add6c4e77fcfffee59
darrenkuro <odon5ht@gmail.com>
2025-12-25 19:29:41 +0100

    Minor improvement on register size

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -3,13 +3,13 @@ extern printf
 
 ; Outside comment
 section .rodata
-code db "global main%1$cextern printf%1$c%1$c; Outside comment%1$csection .rodata%1$ccode db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cmain:%1$c    lea rdi, [rel code]%1$c    mov rsi, 10%1$c    mov rdx, 34%1$c    lea rcx, [rel code]%1$c    xor eax, eax ; Variadic # of XMM%1$c    sub rsp, 8 ; Alignment%1$c    call printf wrt ..plt%1$c    add rsp, 8%1$c    xor eax, eax%1$c    ret%1$c", 0
+code db "global main%1$cextern printf%1$c%1$c; Outside comment%1$csection .rodata%1$ccode db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cmain:%1$c    lea rdi, [rel code]%1$c    mov esi, 10%1$c    mov edx, 34%1$c    lea rcx, [rel code]%1$c    xor eax, eax ; Variadic # of XMM%1$c    sub rsp, 8 ; Alignment%1$c    call printf wrt ..plt%1$c    add rsp, 8%1$c    xor eax, eax%1$c    ret%1$c", 0
 
 section .text
 main:
     lea rdi, [rel code]
-    mov rsi, 10
-    mov rdx, 34
+    mov esi, 10
+    mov edx, 34
     lea rcx, [rel code]
     xor eax, eax ; Variadic # of XMM
     sub rsp, 8 ; Alignment
